e4cc51b5341b2fe918ab8554056a55b39bd61966
salmaaelnaggarr <132565382+salmaaelnaggarr@users.noreply.github.com>
2023-05-05 15:39:45 +0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,200 @@
+$(document).ready(function(){
+    $(".b17").click(function(){
+        $(".tm-figure-description17").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b2").click(function(){
+        $(".tm-figure-description2").toggle(1000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".b3").click(function(){
+        $(".tm-figure-description3").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b4").click(function(){
+        $(".tm-figure-description4").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b1").click(function(){
+        $(".tm-figure-description1").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b5").click(function(){
+        $(".tm-figure-description5").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b6").click(function(){
+        $(".tm-figure-description6").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b7").click(function(){
+        $(".tm-figure-description7").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b8").click(function(){
+        $(".tm-figure-description8").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b9").click(function(){
+        $(".tm-figure-description9").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b10").click(function(){
+        $(".tm-figure-description10").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b11").click(function(){
+        $(".tm-figure-description11").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b12").click(function(){
+        $(".tm-figure-description12").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b13").click(function(){
+        $(".tm-figure-description13").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b14").click(function(){
+        $(".tm-figure-description14").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b15").click(function(){
+        $(".tm-figure-description15").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b16").click(function(){
+        $(".tm-figure-description16").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b18").click(function(){
+        $(".tm-figure-description18").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b19").click(function(){
+        $(".tm-figure-description19").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b20").click(function(){
+        $(".tm-figure-description20").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b21").click(function(){
+        $(".tm-figure-description21").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b22").click(function(){
+        $(".tm-figure-description22").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b23").click(function(){
+        $(".tm-figure-description23").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b24").click(function(){
+        $(".tm-figure-description24").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b25").click(function(){
+        $(".tm-figure-description25").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b26").click(function(){
+        $(".tm-figure-description26").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b27").click(function(){
+        $(".tm-figure-description27").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b28").click(function(){
+        $(".tm-figure-description28").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b29").click(function(){
+        $(".tm-figure-description29").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b30").click(function(){
+        $(".tm-figure-description30").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b31").click(function(){
+        $(".tm-figure-description31").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b32").click(function(){
+        $(".tm-figure-description32").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".b33").click(function(){
+        $(".tm-figure-description33").toggle(1000);
+    });
+});
+
+
